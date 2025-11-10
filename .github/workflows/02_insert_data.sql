dd3941c361b38637bb4f44830e2219ade3ebade2
aldogg10 <179862@upslp.edu.mx>
2025-11-10 17:46:46 -0600

    Delete .github/workflows/02_insert_data.sql

--- .github/workflows/02_insert_data.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO productos VALUES
-(1,'Teclado', 20.99),
-(2,'Television', 50.50),
-(3,'Reloj', 270.00),
-(4,'Silla', 8500.00),
-(5,'Laptop', 999.99);
-
-INSERT INTO departamentos VALUES
-(1, 20);
