5aabdcf9606350cadb81841fb2e4db657f57af5d
aldogg10 <179862@upslp.edu.mx>
2025-11-10 19:08:23 -0600

    Update 02_insert_data.sql

--- 02_insert_data.sql
+++ 02_insert_data.sql
@@ -1,9 +1,9 @@
 INSERT INTO productos VALUES
-(1,'Teclado', 20.99),
-(2,'Television', 50.50),
-(3,'Reloj', 270.00),
-(4,'Silla', 8500.00),
-(5,'Laptop', 999.99);
+(1,'Teclado', 20.99, 10),
+(2,'Television', 50.50, 20),
+(3,'Reloj', 270.00, 30),
+(4,'Silla', 8500.00, 40),
+(5,'Laptop', 999.99, 50);
 
 INSERT INTO departamentos VALUES
 (1, 20);
